f365ede2c54403569ff65112f28c61b9bab66ba8
minjung <clswjf01@gmail.com>
2019-02-24 21:10:48 +0900

    lesson 5_4

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
 from django.utils import timezone
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects #model로 부터 객체 목록을 받아오는 .objects 
     #쿼리셋 #메소드
-
-    return render(request, 'home.html',{'blogs':blogs})
+    #블로그 모든 글들을 대상으로
+    blog_list = Blog.objects.all()
+    #블로그 객체 세개를 한 페이지로 자르기
+    paginator = Paginator(blog_list,3)
+    #request된 페이지가 뭔지를 알아내고
+    page = request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return 해 준다
+    posts = paginator.get_page(page)
+    return render(request, 'home.html',{'blogs':blogs,'posts':posts})
 
 def detail(request,blog_id):
     blog_detail = get_object_or_404(Blog, pk = blog_id)
--- secondproject/urls.py
+++ secondproject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 import blog.views
 import portfolio.views
+import accounts.views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
@@ -24,5 +25,6 @@ urlpatterns = [
     path('',blog.views.home, name="home"),
     path('portfolio/',portfolio.views.portfolio,name='portfolio'),
     path('blog/',include('blog.urls')),
+    path('accounts/',include('accounts.urls'))
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
